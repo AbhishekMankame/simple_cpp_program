0e8bfed6f04b3be2d493f4c32d2bd5f7ef10a9b6
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2022-09-27 00:01:56 +0530

    basic commit

---
+++ loops/ABCD_pattern.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+
+    for(int i=1;i<=n;i++)
+    {
+        char letter='A';
+        // letters n-i+1 letters in inc values
+        for(int cnt=1; cnt<=n-i+1;cnt++){
+            cout<< letter;
+            letter++; // letter=letter+1
+        }
+        // letters n-i+1 letters in dec values
+        letter=letter-1;  //reset the value to prev printed layer
+        for(int cnt=1;cnt<=n-i+1;cnt++){
+            cout<<letter;
+            letter--;
+        }
+        cout<<endl;
+
+    }
+
+
+    return 0;
+}
