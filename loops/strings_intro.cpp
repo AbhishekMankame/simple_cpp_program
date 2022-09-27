506dce5619e1da2573996727177a9ee4d3318630
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2022-09-27 18:51:01 +0530

    Add files via upload

---
+++ loops/strings_intro.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string str = "Hello";
+    string str2;
+    cin>>str2;
+    cout<<str<<endl;
+    cout<<str2<<endl;
+
+    return 0;
+}
