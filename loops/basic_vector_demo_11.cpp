45e13ba42f5476ddf4d350c19dd9a16ae6527ef4
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2022-10-13 14:19:13 +0530

    vector_commit

---
+++ loops/basic_vector_demo_11.cpp
@@ -0,0 +1,14 @@
+// C++ vectors demonstration
+
+#include<iostream>
+#include<vector>
+
+using namespace std;
+int main(){
+	//Demo Vector
+	vector<int> arr = {1,2};
+	//vector<int> a = {1,2,3,4,5};
+	//cout<<sizeof(arr)<<endl;
+	cout<<arr.size()<<endl;
+	//cout<<a.size()<<endl;
+}
