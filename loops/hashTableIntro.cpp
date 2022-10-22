8ee308103202b9a319a433ba4c34653f64648d6f
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2022-10-22 22:37:15 +0530

    hash_table_intro

---
+++ loops/hashTableIntro.cpp
@@ -0,0 +1,15 @@
+//insert (Pizza,199) can be inserted at O(1) time
+//Hash table also supports search operation
+
+//For example if we search the data which is in the list such as dosa it would be
+/*
+search(dosa)-->100   ---- this also takes place in O(1) time
+erase(MAggi)----> it will delete Maggi from the key value pair
+
+Great thing is hash table does these things in O(1) time which is really powerful
+ */
+
+
+//In Hashtable there is no worst case, in most of the cases nearly for 99% it falls on the average case
+
+//In and average it falls in O(1) time and in worst case 
