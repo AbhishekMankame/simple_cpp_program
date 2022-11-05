351127b8986de5932ad83848eb42fc0c53351004
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2022-11-05 23:16:18 +0530

    basic

---
+++ loops/basic_intro_of_cpp4.cpp
@@ -0,0 +1,33 @@
+// Solving the first cpp programming question 
+
+/*
+Given rectanhle of length l and b. Print area of rectangle.
+
+Constraints:
+1 <= l <= 10^9
+1 <= b <= 10^9
+
+Input format:
+2 space seperated integer l and b
+
+Output Format:
+Single number which is area of rectangle
+
+Sample input:
+3 4
+
+Sample output:
+12
+
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+   long long int l,b;
+   cin>>l>>b;
+   cout<<l*b<<endl;
+
+   return 0;
+}
