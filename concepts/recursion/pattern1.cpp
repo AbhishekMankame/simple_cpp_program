b9469ede09c9a2e3d63ac4f3f7f3d3a5714bb6de
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2022-12-20 19:54:43 +0530

    simple_pattern_program

--- concepts/recursion/pattern1.cpp
+++ concepts/recursion/pattern1.cpp
@@ -1,1 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+int main(){
+  int n;
+  cin>>n;
+  int i=0;
+  while(i<=n)
+  {
+    int j=0;
+    while(j<=n){
+      cout<<i;
+      j++;
+    }
+    cout<<endl;
+    i++;
+  }
+  return 0;
+}
