e7878d393e21a1e6490bc35e08eabd137ee75fa4
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2022-12-31 17:07:16 +0000

    simple_pattern_commit

--- concepts/patterns/Pattern12.cpp
+++ concepts/patterns/Pattern12.cpp
@@ -1,1 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
 
+int main(){
+    freopen("input.text","r",stdin);
+    freopen("output.txt","w",stdout);
+    freopen("error.txt","w",stderr);
+
+    ll n;
+    cin>>n;
+    ll i=1;
+    while(i<=n){
+        ll j=1;
+        while(j<=i){
+            cout<<"* ";
+            j++;
+        }
+        cout<<endl;
+        i++;
+    }
+
+    return 0;
+}
