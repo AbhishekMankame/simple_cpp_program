6de900bcc5f535a16c306c477fec2b2d296befea
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2023-01-02 05:25:50 +0000

    pattern18.cpp

---
+++ concepts/patterns/pattern18.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+
+int main(){
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    freopen("error.txt","w",stderr);
+    ll n;
+    cin>>n;
+    ll i=1;
+    char ch='A';
+    while(i<=n){
+        ll j=1;
+        while(j<=i){
+            cout<<ch;
+            ch++;
+            j++;
+        }
+        cout<<endl;
+        i++;
+    }
+    return 0;
+}
