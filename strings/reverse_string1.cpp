f19440d184bd1b8d95e25b01886d63d8629aaae3
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2023-01-05 15:25:08 +0000

    strings_topic

---
+++ strings/reverse_string1.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+
+int main(){
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    freopen("error.txt","w",stderr);
+    string str;
+    cin>>str;
+    string str_rev;
+    for(int i=str.size()-1;i>=0;--i){
+        str_rev=str_rev+str[i];
+    }
+    cout<<str_rev<<endl;
+    return 0;
+}
