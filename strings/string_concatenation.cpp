a29380855bee8a47c615a1d723a034431894baf3
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2023-01-06 03:07:42 +0000

    string_concatenation

---
+++ strings/string_concatenation.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    freopen("error.txt","w",stderr);
+
+    string s1,s2;
+    cin>>s1>>s2;
+    cout<<s1+s2<<endl;
+    cout<<s1+" "+s2<<endl;
+
+    return 0;
+}
