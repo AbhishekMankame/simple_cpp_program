c3b08e3e59398ec879a0c4b5e2b1a3e70374b833
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2023-01-06 07:07:46 +0000

    triangle_pattern

--- concepts/patterns/pattern22.cpp
+++ concepts/patterns/pattern22.cpp
@@ -3,3 +3,27 @@ A
 BB
 CCC
 */
+
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+
+int main(){
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    freopen("error.txt","w",stderr);
+    ll n;
+    cin>>n;
+    ll i=1;
+    while(i<=n){
+        ll j=1;
+        while(j<=i){
+            char ch=i-1+'A';
+            cout<<ch;
+            j++;
+        }
+        cout<<endl;
+        i++;
+    }
+    return 0;
+}
