36d8a055bb6cdc877c190b47f7d25661f4f07d6c
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2023-01-06 02:46:39 +0000

    cin.ignore

--- strings/cin_getline2.cpp
+++ strings/cin_getline2.cpp
@@ -1,12 +1,18 @@
 #include<bits/stdc++.h>
 using namespace std;
+#define ll long long int
 
 int main(){
     freopen("input.txt","r",stdin);
     freopen("output.txt","w",stdout);
     freopen("error.txt","w",stderr);
-    string s;
-    getline(cin,s);
-    cout<<s<<endl;
+    ll t;
+    cin>>t;
+    cin.ignore();
+    while(t--){
+        string s;
+        getline(cin,s);
+        cout<<s<<endl;
+    }
     return 0;
 }
