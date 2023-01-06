f4107dd2cbc9d460c4f869ca259d80f9f23f6260
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2023-01-06 13:00:15 +0000

    rev_double_pattern

---
+++ concepts/patterns/pattern28.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+
+int main(){
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    freopen("error.txt","w",stderr);
+     ll n;
+     cin>>n;
+     ll i=n;
+     while(i>0){
+        ll space=n-i+1;
+        while(space){
+            cout<<" ";
+            space--;
+        }
+        ll j=1;
+        ll k=1;
+        while(j<=i){
+            cout<<"*";
+            j++;
+        }
+        while(k<=i){
+            cout<<"*";
+            k++;
+        }
+
+
+        cout<<endl;
+        i--;
+     }
+     return 0;
+}
