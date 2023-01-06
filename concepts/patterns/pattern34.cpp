89bbe6c7f248fdf23779db63f66267b4d6835088
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2023-01-06 15:39:06 +0000

    compund_pattern

---
+++ concepts/patterns/pattern34.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+
+int main(){
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    freopen("error.txt","w",stderr);
+
+    ll n;
+    cin>>n;
+    int i=n;
+    while(i>0){
+        ll j=1;
+        ll j1=i;
+        while(j<=i){
+            cout<<j;
+            j++;
+        }
+        int star=n-i;
+        int star1=n-i;
+        while(star){
+            cout<<"*";
+            star--;
+        }
+        while(star1){
+            cout<<"*";
+            star1--;
+        }
+        while(j1>0){
+            cout<<j1;
+            j1--;
+        }
+
+
+        cout<<endl;
+        i--;
+    }
+}
