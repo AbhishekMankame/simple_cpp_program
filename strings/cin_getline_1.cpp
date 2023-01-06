c8bf84fab1eeab125e7f24cbb7953c224ba3267f
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2023-01-06 02:41:56 +0000

    string_getline

---
+++ strings/cin_getline_1.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    freopen("error.txt","w",stderr);
+    string s;
+    getline(cin,s);
+    cout<<s<<endl;
+    //
+}
