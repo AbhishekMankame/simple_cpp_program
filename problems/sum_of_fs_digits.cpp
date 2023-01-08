ca5a6575398a3046c61642501543872ae95ca4a2
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2023-01-08 17:18:54 +0000

    fl_sum_simple

---
+++ problems/sum_of_fs_digits.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+
+int main(){
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    freopen("error.txt","w",stderr);
+    ll t;
+    cin>>t;
+    while(t--){
+        int num, rem;
+        cin>>num;
+        rem=num%10;
+        while(num>9){
+            num/=10;
+        }
+        cout<<num+rem<<endl;
+    }
+
+    return 0;
+}
