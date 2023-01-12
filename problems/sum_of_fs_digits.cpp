dfc1e12c929a3349ec95f49dfd06ddd009265be6
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2023-01-12 12:33:00 +0000

    simple_comments

--- problems/sum_of_fs_digits.cpp
+++ problems/sum_of_fs_digits.cpp
@@ -6,12 +6,12 @@ int main(){
     freopen("input.txt","r",stdin);
     freopen("output.txt","w",stdout);
     freopen("error.txt","w",stderr);
-    ll t;
+    ll t;                   // test cases
     cin>>t;
     while(t--){
         ll num, rem;
         cin>>num;
-        rem=num%10;
+        rem=num%10;         //remainder
         while(num>9){
             num/=10;
         }
