ce7c06b57b0747d54f98155e66ca8e5912f2fcc8
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2023-01-21 14:25:32 +0000

    reverse_array

---
+++ problems/arrays/reverse_array.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void reverseArray(int arr[], int n){
+    int s=0;
+    int e=n-1;
+    while(s<=e){
+        swap(arr[s],arr[e]);
+        s++;
+        e--;
+    }
+}
+
+int main(){
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    freopen("error.txt","w",stderr);
+
+    int a[]={12,2,34,43,53,655,6,7};
+    int n=sizeof(a)/sizeof(int);
+    reverseArray(a,n);
+    for(auto i:a){
+        cout<<i<<" ";
+    }
+    cout<<endl;
+
+    return 0;
+}
