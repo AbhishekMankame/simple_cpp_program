355f56b1e253687426bf4926c78075f29ee1a875
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2023-01-21 05:11:38 +0000

    intro_to_array

---
+++ problems/arrays/intro_to_array.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define endl '\n'
+
+int main(){
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    freopen("error.txt","w",stderr);
+    int a[]={1,2,3,4};
+    int n=sizeof(a)/sizeof(int);
+    cout<<n<<endl;
+
+    return 0;
+}
