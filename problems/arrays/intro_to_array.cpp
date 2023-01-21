c7c9fe838cf26e698e6cc5d4c8e5cf2e6f3c4882
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2023-01-21 05:24:04 +0000

    simple_update

--- problems/arrays/intro_to_array.cpp
+++ problems/arrays/intro_to_array.cpp
@@ -3,13 +3,18 @@ using namespace std;
 #define ll long long int
 #define endl '\n'
 
+void function1(int arr[]){
+    cout<<"In function size of array is "<<sizeof(arr)<<endl;
+}
+
 int main(){
     freopen("input.txt","r",stdin);
     freopen("output.txt","w",stdout);
     freopen("error.txt","w",stderr);
-    int a[]={1,2,3,4};
+    int a[]={1,2,3,4,5,6};
     int n=sizeof(a)/sizeof(int);
-    cout<<n<<endl;
-
+    cout<<"In main "<<n<<endl;
+    cout<<"Size of array in Main "<<sizeof(a)<<endl;
+    function1(a);
     return 0;
 }
