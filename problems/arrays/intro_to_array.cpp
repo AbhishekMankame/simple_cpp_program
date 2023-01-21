7d0f8674fcbf2c2ba0b5ad627a566c7786a0c4fb
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2023-01-21 07:47:56 +0000

    simple_updates

--- problems/arrays/intro_to_array.cpp
+++ problems/arrays/intro_to_array.cpp
@@ -3,8 +3,16 @@ using namespace std;
 #define ll long long int
 #define endl '\n'
 
-void function1(int arr[]){
+// Here in this case we wii get the output as 8, s we need to specify the size of the array
+// void function1(int arr[]){
+//     cout<<"In function size of array is "<<sizeof(arr)<<endl;
+// }
+
+void function2(int arr[], int n){
     cout<<"In function size of array is "<<sizeof(arr)<<endl;
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<endl;
+    }
 }
 
 int main(){
@@ -15,6 +23,6 @@ int main(){
     int n=sizeof(a)/sizeof(int);
     cout<<"In main "<<n<<endl;
     cout<<"Size of array in Main "<<sizeof(a)<<endl;
-    function1(a);
+    function2(a,n);
     return 0;
 }
