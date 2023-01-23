dec11774c7553c6afbb892e2a31d0a9c77c439c0
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2023-01-23 18:29:22 +0000

    bit++_solution1

---
+++ problems/problem_set/bit++_solution01.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include<cstdio>
+#include<algorithm>
+#include<cmath>
+#include<cstring>
+using namespace std;
+char str[100];
+int main()
+{
+    int i,j,t;
+    int n,m,num,max=0;
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    scanf("%d",&n);
+    j=0;
+    for(i=0;i<n;++i)
+    {
+        scanf("%s",str);
+        int l=strlen(str);
+        for(t=0;t<l;++t)
+        {
+            if(str[t]=='+'&&str[t+1]=='+')j++;
+            if(str[t]=='-'&&str[t+1]=='-')j--;
+        }
+    }
+    printf("%d",j);
+ 
+ 
+ 
+   return 0;
+}
