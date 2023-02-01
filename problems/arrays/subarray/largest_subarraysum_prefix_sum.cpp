e1a664099d83ffdf750d7025810bb671af289463
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2023-02-01 17:34:59 +0000

    simple_update

---
+++ problems/arrays/subarray/largest_subarraysum_prefix_sum.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+
+int main(){
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    freopen("error.txt","w",stderr);
+    freopen("error.txt","w",stderr);
+    int arr[]={-2,3,4,-1,5,-12,6,1,3};
+    int n=sizeof(arr)/sizeof(int);
+    cout<<largestSubarraySum(arr,n)<<endl;
+}
