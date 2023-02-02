d7d65672f1678e002b3467c1139d72c54ff7fc35
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2023-02-02 15:54:27 +0000

    kadane_algorithm

---
+++ problems/arrays/subarray/largest_subarraysum_kadane_algorithm.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int largestSubarraySum(int arr[],int n){
+    int cs=0;
+    int ls=0;
+    for(int i=0;i<n;i++){
+        cs=cs+arr[i];
+        if(cs<0){
+            cs=0;
+        }
+        ls=max(ls,cs);
+    }
+    return ls;
+}
+
+int main(){
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    freopen("error.txt","w",stderr);
+    freopen("error.txt","w",stderr);
+    int arr[]={-2,3,4,-1,5,-12,6,1,3};
+    int n=sizeof(arr)/sizeof(int);
+    cout<<largestSubarraySum(arr,n)<<endl;
+}
