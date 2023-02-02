88f600a763356e23618dddade89c7c7afab98513
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2023-02-02 10:54:39 +0000

    prefix_sum

--- problems/arrays/subarray/largest_subarraysum_prefix_sum.cpp
+++ problems/arrays/subarray/largest_subarraysum_prefix_sum.cpp
@@ -3,7 +3,19 @@ using namespace std;
 #define endl '\n'
 
 int largestSubarraySum(int arr[], int n){
-    
+    int prefix[100]={0};
+    prefix[0]=arr[0];
+    for(int i=1;i<n;i++){
+        prefix[i]=prefix[i-1]+arr[i];
+    }
+    int largest_sum=0;
+    for(int i=0;i<n;i++){
+        for(int j=i;j<n;j++){
+            int subarraysum = i>0?prefix[j]-prefix[i-1] : prefix[j];
+            largest_sum=max(largest_sum,subarraysum);
+        }
+    }
+    return largest_sum;
 }
 
 int main(){
