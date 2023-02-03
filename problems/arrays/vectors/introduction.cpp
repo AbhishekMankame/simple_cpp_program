c3f20e135ad027dea2a82826e36226dafcea9a16
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2023-02-03 04:42:44 +0000

    basic_code

---
+++ problems/arrays/vectors/introduction.cpp
@@ -0,0 +1,15 @@
+// Vector demo
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main(){
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    freopen("error.txt", "w", stderr);
+    // Demo vector
+    vector<int> arr;
+    cout<<arr.size()<<endl;
+
+    return 0;
+}
