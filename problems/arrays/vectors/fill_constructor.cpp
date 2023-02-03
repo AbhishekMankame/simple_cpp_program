13592faa8a0f128333db5a3fabf8893c92f13bd3
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2023-02-03 11:30:22 +0000

    fill_constructor

---
+++ problems/arrays/vectors/fill_constructor.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    freopen("error.txt","w",stderr);
+    vector<int> arr(10,7);
+    for(int i=0;i<arr.size();i++){
+        cout<<arr[i]<<endl;
+    }
+    return 0;
+}
