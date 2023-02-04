aff428990245fc2cc7530ed2dc4ac79fd9643222
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2023-02-04 19:00:12 +0000

    simple_update

--- problems/arrays/sorting/bubble_sort.cpp
+++ problems/arrays/sorting/bubble_sort.cpp
@@ -2,12 +2,12 @@
 using namespace std;
 #define ll long long int
 
-int bubblesort(int arr[], int n){
+void bubblesort(int arr[], int n){
     for(int times=1;times<=n-1;times++){
         //do repeated swapping
         for(int j=0;j<=n-times-1;j++){
-            if(a[j]>a[j+1]){
-                swap(a[j],a[j+1]);
+            if(arr[j]>arr[j+1]){
+                swap(arr[j],arr[j+1]);
             }
         }
     }
@@ -20,5 +20,9 @@ int main(){
 
     int arr[]={-1,1,23,4,121,42,3,56,54,3,332};
     int n=sizeof(arr)/sizeof(int);
+    bubblesort(arr,n);
+    for(auto x:arr){
+        cout<<x<<",";
+    }
 
 }
