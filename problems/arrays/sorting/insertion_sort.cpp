a94b827459930fac7ec6be64722245aafb0cb0b1
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2023-02-05 05:27:24 +0000

    simple_commit

--- problems/arrays/sorting/insertion_sort.cpp
+++ problems/arrays/sorting/insertion_sort.cpp
@@ -5,6 +5,8 @@ void insertionsort(int arr[], int n){
     for(int i=0;i<n;i++){
         int current=arr[i];
         int prev=i-1;
+
+        //loop to find the right index where the element current should be inserted
         while(prev>0 and arr[prev]>current){
             arr[prev+1]=arr[prev];
             prev=prev-1;
@@ -20,4 +22,8 @@ int main(){
 
     int arr[]={-3,2,32,23,-43,134,5,53,12,567};
     int n=sizeof(arr)/sizeof(int);
+    insertionsort(arr,n);
+    for(auto x:arr){
+        cout<<x<<",";
+    }
 }
