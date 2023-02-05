47560ada62240c6c19336084bbd80aff7423b608
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2023-02-05 05:14:16 +0000

    insertion_sort

---
+++ problems/arrays/sorting/insertion_sort.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    freopen("error.txt","w",stderr);
+
+    int arr[]={-3,2,32,23,-43,134,5,53,12,567};
+    int n=sizeof(arr)/sizeof(int);
+}
