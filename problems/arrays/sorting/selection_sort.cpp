17df61784a45216a3d142fb38989a2ee3d4980bd
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2023-02-07 17:14:55 +0000

    simple_update

--- problems/arrays/sorting/selection_sort.cpp
+++ problems/arrays/sorting/selection_sort.cpp
@@ -1,1 +1,24 @@
 // selection sort
+#include<iostream>
+using namespace std;
+
+// selection sort
+
+void selection_sort(int arr[], int n){
+    
+}
+
+int main(){
+#ifndef ONLINE_JUDGE
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    freopen("error.txt","w",stderr);
+#endif
+    int arr[]={10,2,-13,-1,34,54,5,31,64,756,32};
+    int n=sizeof(arr)/sizeof(int);
+    selection_sort(arr,n);
+    for(auto i:arr){
+        cout<<i<<",";
+    }
+
+}
