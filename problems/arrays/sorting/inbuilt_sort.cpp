4b3e45cc6dbecce928dde0ceb72947e70ce0bb96
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2023-02-11 12:56:18 +0000

    sort_function

--- problems/arrays/sorting/inbuilt_sort.cpp
+++ problems/arrays/sorting/inbuilt_sort.cpp
@@ -5,4 +5,13 @@ int main(){
     freopen("input.txt","r",stdin);
     freopen("output.txt","w",stdout);
     freopen("error.txt","w",stderr);
+
+    int arr[]={1,343,4,54,-32,2,-11,443,5,64,43,222};
+    int n=sizeof(arr)/sizeof(int);
+
+    for(auto i:arr){
+        cout<<i<<" ";
+    }
+
+    return 0;
 }
