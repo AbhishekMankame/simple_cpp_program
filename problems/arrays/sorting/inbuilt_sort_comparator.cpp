de18e8e9bfe4b059bae06258cc85ffcb99aa77ad
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2023-02-14 09:52:43 +0000

    simple_commit

---
+++ problems/arrays/sorting/inbuilt_sort_comparator.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    freopen("error.txt","w",stderr);
+
+    int arr[]={21,2,-23,-4,353,4,324,1,-12,0,4,-56};
+    int n=sizeof(arr)/sizeof(int);
+
+    for(auto i:arr){
+        cout<<i<<" ";
+    }
+}
