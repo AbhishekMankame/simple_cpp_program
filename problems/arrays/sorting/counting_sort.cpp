6828eb17b3d2757489b8200be9c45387c431ed0b
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2023-02-16 12:27:54 +0000

    counting_sort

---
+++ problems/arrays/sorting/counting_sort.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+
+void counting_sort(int arr[], int n){
+
+}
+
+int main(){
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    freopen("error.txt","w",stderr);
+
+    int arr[]={21,2,-23,-4,353,4,324,1,-12,0,4,-56};
+    int n=sizeof(arr)/sizeof(int);
+
+    for(auto i:arr){
+        cout<<i<<" ";
+    }
+    cout<<endl;
+}
