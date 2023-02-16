306f0845ccbb10b34aed85d9be35269ef5d8d694
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2023-02-16 17:51:20 +0000

    char_array_introduction

--- problems/arrays/char_array/introduction.cpp
+++ problems/arrays/char_array/introduction.cpp
@@ -1,8 +1,15 @@
 #include<iostream>
 #include<cstring>
+using namespace std;
 
 int main(){
-
+#ifndef ONLINE_JUDGE
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    freopen("error.txt","w",stderr);
+#endif
+    char a[]="abcdefgh";
+    cout<<a<<endl;
 
     return 0;
 }
