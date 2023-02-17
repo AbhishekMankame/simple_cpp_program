dcee287e642d6fcd69d836a848e79dbccf66a4ad
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2023-02-17 09:12:02 +0000

    simple_update

---
+++ problems/arrays/char_array/input_output.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+
+int main(){
+#ifdef ONLINE_JUDGE
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    freopen("error.txt","w",stderr);
+#endif
+}
