f7fbb38a93d19bb343f8c6654caa4a9867f0094a
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2023-03-02 14:27:57 +0000

    simple_update

---
+++ strings/char/cin_get_input.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void fast(){
+    ios_base::sync_with_stdio(0); 
+    cin.tie(0);
+    cout.tie(0);
+}
+
+int main(){
+#ifndef ONLINE_JUDGE
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    freopen("error.txt","w",stderr);
+#endif
+
+    fast();
+    char sentence[1000];
+    char temp=cin.get();
+
+    int len=1;
+
+
+    return 0;
+}
