9333af9587d24e2284201e4004be06e6be75a0ab
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2023-03-03 16:20:16 +0000

    simple_update

---
+++ strings/char/cin_get_input2.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void fast(){
+    
+}
+
+int main(){
+#ifndef ONLINE_JUDGE
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    freopen("error.txt","w",stderr);
+#endif
+    fast();
+}
