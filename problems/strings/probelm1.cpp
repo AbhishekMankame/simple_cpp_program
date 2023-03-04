5b42248860b0962122136534c10333dac7c2cb31
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2023-03-04 16:15:30 +0000

    simple_north_west

--- problems/strings/probelm1.cpp
+++ problems/strings/probelm1.cpp
@@ -51,6 +51,16 @@ void solve(){
             cout<<"E";
         }
     }
+
+    // for North-West
+    else if(x<0 and y>=0){
+        while(y--){
+            cout<<"N";
+        }
+        while(x++){
+            cout<<"W";
+        }
+    }
 }
 
 int main(){
