7b938fc9dee37d2cd21207277dbd574859a737ce
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2023-03-06 17:23:31 +0000

    simple_commit

--- strings/str/intr2.cpp
+++ strings/str/intr2.cpp
@@ -11,10 +11,18 @@ void solve(){
     int n;
     cin>>n;
     char sentence[1000];
+    char largest[1000];
+    int largest_len=0;
     cin.get();
     while(n--){
         cin.getline(sentence,1000);
         cout<<sentence<<endl;
+
+        int len=strlen(sentence);
+        if(len>largest_len){
+            largest_len=len;
+            strcpy(largest,sentence);
+        }
     }
 }
 
