407ea76417171cb0b6dd3f65ffc36f2cbebebe75
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2023-03-07 02:33:57 +0000

    simple_commit

--- strings/str/run_length_encoding.cpp
+++ strings/str/run_length_encoding.cpp
@@ -17,7 +17,15 @@ void fast(){
 
 string compression(string str){
     int n=str.length();
-    int count=1;
+    string output;
+
+    for(int i=0;i<n;i++){
+        int count=1;
+        while(i<n-1 and str[i+1]==str[i]){
+            count++;
+            i++;
+        }
+    }
 }
 
 void solve(){
