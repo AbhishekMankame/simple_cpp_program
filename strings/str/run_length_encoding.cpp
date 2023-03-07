ee998f1d2bb3eaabe8a130dac4a7a2dab7709bc5
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2023-03-07 02:19:33 +0000

    simple_commit

---
+++ strings/str/run_length_encoding.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void fast(){
+    ios_base::sync_with_stdio(0); 
+    cin.tie(0); 
+    cout.tie(0);
+}
+
+void solve(){
+
+}
+
+int main(){
+#ifndef ONLINE_JUDGE
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    freopen("error.txt","w",stderr);
+#endif
+
+    fast();
+    solve();
+
+    
+    return 0;
+}
