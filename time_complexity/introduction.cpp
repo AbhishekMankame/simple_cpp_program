b96f018e2bb793c9d32acf307e8597f44003bebe
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2023-03-09 17:51:21 +0000

    simple_commit

--- time_complexity/introduction.cpp
+++ time_complexity/introduction.cpp
@@ -1,4 +1,4 @@
 /* Time Complexity:
-Time complexity is a tool, through using which we calculate or analyize the execution time of the written code 
+Time complexity is a tool, through using which we calculate or analyize or estimate the execution time of the written code 
 
 */
