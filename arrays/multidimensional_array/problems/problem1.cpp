ebd2f8f6856eb0e0f3d83877c39f0f49a41325aa
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2023-03-09 04:20:12 +0000

    problem_details_update

--- arrays/multidimensional_array/problems/problem1.cpp
+++ arrays/multidimensional_array/problems/problem1.cpp
@@ -1,3 +1,18 @@
+/*
+Spiral print:
+Write a function that takes in n x m 2D Array, and prints the all array elements in a spiral order. Start from top left corner goes to right, proceeds in a spiral pattern until every element in visited.
+
+Input:
+1 2 3 4
+5 6 7 8
+9 10 11 12
+13 14 15 16
+
+Output:
+1 2 3 4 8 12 16 15 14 13 9 5 6 7 11 10
+
+*/
+
 #include<bits/stdc++.h>
 using namespace std;
 
