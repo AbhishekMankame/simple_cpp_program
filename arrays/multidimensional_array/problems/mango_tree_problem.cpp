fb533fec8e52fadccaf90b77d23218b1de208208
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2023-03-09 16:35:07 +0000

    input_update

--- arrays/multidimensional_array/problems/mango_tree_problem.cpp
+++ arrays/multidimensional_array/problems/mango_tree_problem.cpp
@@ -3,12 +3,13 @@ Mango trees:
 Rahul's father has left a farm organized as an N X N grid. Each square in the grid either has or does not have a mango tree. He has to divide they farm with his three sisters as follows: he will draw one horizontal line and one vertical line to divide the field into four rectangles. His friends will choose three of the four smaller fields and he gets the last one.
 
 Input:
-.##...
-#..##.
-.#....
-.##..#
-#..#..
-.#....
+. # # . . .
+# . . # # .
+. # . . . .
+. # # . . #
+# . . # . .
+. # . . . .
+
 
 */
 
