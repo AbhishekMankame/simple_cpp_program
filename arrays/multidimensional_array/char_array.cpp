070137514b83cb38a8f12d5e52db53bb804c5bd9
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2023-03-09 04:00:45 +0000

    simple_commit

--- arrays/multidimensional_array/char_array.cpp
+++ arrays/multidimensional_array/char_array.cpp
@@ -10,6 +10,7 @@ void fast(){
 void solve(){
     char numbers[][10]={"one", "two", "three"}; // Here each string represents character array
     cout<<numbers[2][1]<<endl;
+    cout<<numbers[1]<<endl;
 
 }
 
